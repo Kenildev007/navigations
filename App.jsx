a17df79403edb499d32fca090c70a04ba5983864
Kenildev007 <kenilgorasya6@gmail.com>
2024-04-04 14:42:55 +0530

    no counter on product detail

--- App.jsx
+++ App.jsx
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './src/redux/store';
 
+
+
 const App = () => {
   return (
     <>
